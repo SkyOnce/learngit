9c6ed5486f81efff00bd25310b0d8b876a18ac25
SkyOnce <592849656@qq.com>
2019-12-26 16:53:01 +0800

    add uint_test.c

---
+++ uint_test.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: uint_test.c
+	> Author: lilonglong
+	> Mail: 592849656@qq.com 
+	> Created Time: Tue 24 Dec 2019 10:31:55 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(void)
+{
+	unsigned int i = 0x00ff0000;
+	printf("----i = %#X\n",i);
+
+	return 0;
+}
